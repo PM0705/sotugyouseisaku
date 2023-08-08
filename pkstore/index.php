af8507e3e31440cf21d0cbe88dc8d4e05214b2f7
岡田ゆき <okadayuki@okadayukis-Air.lan>
2023-08-08 21:16:40 +0900

    スライドショー完成

--- pkstore/index.php
+++ pkstore/index.php
@@ -5,19 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PKstoreWELCOM</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
-    <script class="slide">
-      $(document).ready(function(){
-        $('.abc').bxSlider({
-            auto: true,
-            speed: 2000,
-            
-        });
-      });
-      
-    </script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="css/6-1-7.css">
+    
     <link rel="stylesheet" href="htmlstyle.css">
     
 </head>
@@ -41,20 +31,14 @@
     </div>
  </header>
  <main>
-    <div class="main-container">
-        <div class="left">
-            <div class="slidebox">
-                <div class="abc slide-item">
-                    <div><img src="img/jQuery_image1.jpg" alt="スライド1"></div>
-                    <div><img src="img/jQuery_image2.jpg" alt="スライド2"></div>
-                    <div><img src="img/jQuery_image3.jpg" alt="スライド3"></div>
-                    <div><img src="img/jQuery_image4.jpg" alt="スライド4"></div>
-                    <div><img src="img/jQuery_image5.jpg" alt="スライド5"></div>
-                    
-                </div>
-            </div>
-        </div>
-    </div>
+    <ul class="slider"><!--/slider-->
+        <li><img src="img/slide1.png" alt="スライド1"></li>
+        <li><img src="img/slide2.png" alt="スライド2"></li>
+        <li><img src="img/slide3.png" alt="スライド3"></li>
+        <li><img src="img/slide4.png" alt="スライド4"></li>
+        <li><img src="img/slide5.png" alt="スライド5"></li>
+    </ul>
+        
  </main>
  <footer>
     
@@ -78,6 +62,23 @@
  </footer>
 
     
- 
+ <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <!--自作のJS-->
+    <script>
+    $('.slider').slick({
+		autoplay: true,//自動的に動き出すか。初期値はfalse。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		speed: 500,//スライドのスピード。初期値は300。
+		slidesToShow: 3,//スライドを画面に3枚見せる
+		slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+		centerMode: true,//要素を中央ぞろえにする
+		variableWidth: true,//幅の違う画像の高さを揃えて表示
+		dots: true,//下部ドットナビゲーションの表示
+	});
+    </script>
+    <script src="js/6-1-7.js"></script>
 </body>
 </html>
