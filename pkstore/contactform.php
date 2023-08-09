4bd8ee78e506eb5c9de215dce8040f1c0b4f988d
岡田ゆき <okadayuki@okadayukis-Air.lan>
2023-08-10 02:30:59 +0900

    0809

--- pkstore/contactform.php
+++ pkstore/contactform.php
@@ -31,50 +31,7 @@
     </div>
  </header>
  <main>
-    <ul class="slider"><!--/slider-->
-        <li><img src="img/slide1.png" alt="スライド1"></li>
-        <li><img src="img/slide2.png" alt="スライド2"></li>
-        <li><img src="img/slide3.png" alt="スライド3"></li>
-        <li><img src="img/slide4.png" alt="スライド4"></li>
-        <li><img src="img/slide5.png" alt="スライド5"></li>
-    </ul>
 
-    <h3>NEW〜情報〜</h3>
-    <div class="info">
-        <img src="img/newinfo1.png" alt="newinfo1">
-        <img src="img/newinfo2.png" alt="newinfo2">
-        <img src="img/newinfo3.png" alt="newinfo3">
-        <img src="img/newinfo4.png" alt="newinfo4">
-    </div>
-
-    <h3>NEW〜グッズ〜</h3>
-    <div class="info">
-        <img src="img/newg1.png" alt="newg1">
-        <img src="img/newg2.png" alt="newg2">
-        <img src="img/newg3.png" alt="newg3">
-        <img src="img/newg4.png" alt="newg4">
-    </div>
-    <h3>ランキング</h3>
-    <div class="info">
-        <img src="img/rank1.png" alt="rank1">
-        <img src="img/rank2.png" alt="rank2">
-        <img src="img/rank3.png" alt="rank3">
-        <img src="img/rank4.png" alt="rank4">
-    </div>
-    <h3>店舗情報</h3>
-    <div class="mapinfo">
-        <div class="text-info">
-            <p class="title">住所</p>
-            <p class="add">〒164-8501<br>東京都中野区中野四丁目8番1号<br></p>
-            <p class="add2"><br>TEL<br>000-0000-0000<br>
-                            営業時間<br>10:00~21:00</p>
-        </div>
-        <div class="googlemap">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3239.740446698844!2d139.661134012126!3d35.70800427246428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f29092a6e253%3A0x936cd62aa3dafab6!2z44CSMTY0LTAwMDEg5p2x5Lqs6YO95Lit6YeO5Yy65Lit6YeO77yU5LiB55uu77yY4oiS77yY!5e0!3m2!1sja!2sjp!4v1691581831273!5m2!1sja!2sjp"
-                style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-    </div>
-        
  </main>
  <footer>
     
@@ -87,14 +44,12 @@
         </ul>
     </div>
     <div class="footer-r">
-        
+        <p>OFFICIAL SNS</p>
         <ul>
-            <a href="index.php"><img src="img/twittericon.png" alt="Xのロゴ" class="img1"></a>
-            <a href="index.php"><img src="img/instaicon.png" alt="Instagramのロゴ" class="img1"></a>
-            <a href="index.php"><img src="img/youtubeicon.png" alt="Youtubeのロゴ" class="img1 youtubeicon"></a>
-            
+                <li><a href="index.php">X（旧twitter）</a></li>
+                <li><a href="index.php">Instagram</a></li>
+                <li><a href="index.php">Youtube</a></li>
         </ul>
-        <p>OFFICIALSNS</p>
     </div>
     
  </footer>
