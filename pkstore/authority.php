37790d27feebe2d92e44a56297902d6d18de3198
岡田ゆき <okadayuki@okadayukis-Air.lan>
2023-08-14 23:42:30 +0900

    管理者メニュ

--- pkstore/authority.php
+++ pkstore/authority.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PKstoreWELCOM</title>
+    <title>管理者メニュー</title>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
     <link rel="stylesheet" type="text/css" href="css/6-1-7.css">
     
@@ -18,7 +18,7 @@
         <a href="index.php"><img src="img/logo.png" alt="PKstoreのロゴ" class="img"></a>
         <img src="img/character.png" alt="PKstoreのキャラクター" class="img">
         <a href="buyItem.php">カートの中身（仮）</a>
-        <a href="authority.php">管理者用メニュー（仮）</a>
+        <a href="buyItem.php">管理者用メニュー（仮）</a>
     </div>
     
     <div class="header-right">
@@ -34,64 +34,16 @@
     </div>
  </header>
  <main>
-    <ul class="slider"><!--/slider-->
-        <li><img src="img/slide1.png" alt="スライド1"></li>
-        <li><img src="img/slide2.png" alt="スライド2"></li>
-        <li><img src="img/slide3.png" alt="スライド3"></li>
-        <li><img src="img/slide4.png" alt="スライド4"></li>
-        <li><img src="img/slide5.png" alt="スライド5"></li>
-    </ul>
+    <h3>管理者メニュー</h3>
+    <a href="buyItem.php" class="authority">商品登録</a>
+    <a href="buyItem.php" class="authority">商品編集</a>
+    <a href="buyItem.php" class="authority">情報登録</a>
+    <a href="buyItem.php" class="authority">スライドショー編集</a>
+    <a href="buyItem.php" class="authority">会員リスト（特別管理者用）</a>
+    <a href="buyItem.php" class="authority">お問い合わせ受信リスト</a>
 
-    <h3>NEW〜情報〜</h3>
-    <div class="info">
-        <img src="img/newinfo1.png" alt="newinfo1">
-        <img src="img/newinfo2.png" alt="newinfo2">
-        <img src="img/newinfo3.png" alt="newinfo3">
-        <img src="img/newinfo4.png" alt="newinfo4">
-    </div>
 
-    <h3>NEW〜グッズ〜</h3>
-    <div class="info">
-        <div class="relative">
-            <a href="new-goods.php"><img src="img/newg1.png" alt="newg1" class="info-img"></a>
-            <img src="img/newIcon.png" alt="newIcon" class="absolute">
-        </div>
-        <div class="relative">
-            <a href="new-goods.php"><img src="img/newg2.png" alt="newg2" class="info-img"></a>
-            <img src="img/newIcon.png" alt="newIcon" class="absolute">
-        </div>
-        <div class="relative">
-            <a href="new-goods.php"><img src="img/newg3.png" alt="newg3" class="info-img"></a>
-            <img src="img/newIcon.png" alt="newIcon" class="absolute">
-        </div>
-        <div class="relative">
-            <a href="new-goods.php"><img src="img/newg4.png" alt="newg4" class="info-img"></a>
-            <img src="img/newIcon.png" alt="newIcon" class="absolute">
-        </div>
-        
-        
-    </div>
-    <h3>ランキング</h3>
-    <div class="info">
-        <img src="img/rank1.png" alt="rank1">
-        <img src="img/rank2.png" alt="rank2">
-        <img src="img/rank3.png" alt="rank3">
-        <img src="img/rank4.png" alt="rank4">
-    </div>
-    <h3>店舗情報</h3>
-    <div class="mapinfo">
-        <div class="text-info">
-            <p class="title">住所</p>
-            <p class="add">〒164-8501<br>東京都中野区中野四丁目8番1号<br></p>
-            <p class="add2"><br>TEL<br>000-0000-0000<br>
-                            営業時間<br>10:00~21:00</p>
-        </div>
-        <div class="googlemap">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3239.740446698844!2d139.661134012126!3d35.70800427246428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f29092a6e253%3A0x936cd62aa3dafab6!2z44CSMTY0LTAwMDEg5p2x5Lqs6YO95Lit6YeO5Yy65Lit6YeO77yU5LiB55uu77yY4oiS77yY!5e0!3m2!1sja!2sjp!4v1691581831273!5m2!1sja!2sjp"
-                style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-    </div>
-        
+
  </main>
  <footer>
     
