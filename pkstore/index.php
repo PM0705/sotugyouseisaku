49873f5600b5539e6c3245341b35d58dbaebd786
岡田ゆき <okadayuki@PKs-MacBook-Air.lan>
2023-10-03 21:50:21 +0900

    右カラム画像の大きさ

--- pkstore/index.php
+++ pkstore/index.php
@@ -262,7 +262,7 @@ $stmt_category = $pdo->query("SELECT * FROM item_info_transaction where delete_f
             infinite: true, //スライドをループさせるかどうか。初期値はtrue。
             speed: 500, //スライドのスピード。初期値は300。
             slidesToShow: 1, //スライドを画面に3枚見せる
-            slidesToScroll: 3, //1回のスクロールで1枚の写真を移動して見せる
+            slidesToScroll: 1, //1回のスクロールで1枚の写真を移動して見せる
             prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
             nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
             centerMode: true, //要素を中央ぞろえにする
