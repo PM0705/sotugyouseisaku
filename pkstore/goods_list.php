82edec658189af1da4bfb4a98e6b6a5a8a5f6a3f
岡田ゆき <okadayuki@PKs-MacBook-Air.lan>
2023-10-03 22:34:05 +0900

    ファビコン

--- pkstore/goods_list.php
+++ pkstore/goods_list.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>商品編集</title>
+    <link rel="icon" href="img/favicon.ico" id="favicon">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon-180x180.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
